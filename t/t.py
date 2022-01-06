d20d8be805226c3484fdf32c5b871d7aa58cb97b
s045pd <s045pd.x@gmail.com>
2022-01-06 22:31:36 +0800

    add offline && new color

--- t/t.py
+++ t/t.py
@@ -1,12 +1,17 @@
+import sys
 from dataclasses import dataclass
+from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 import click
 import requests_html
 from playsound import playsound
-from prompt_toolkit import PromptSession, prompt
+from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
-from prompt_toolkit.completion import Completer, Completion
+from prompt_toolkit.completion import Completer, Completion, WordCompleter
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.styles import Style
+
 from termcolor import colored
 
 from t.common import check_langs, check_platform
@@ -15,33 +20,46 @@ from t.extract import Parser
 # WORKER, LANGS = check_langs()
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+style = Style.from_dict(
+    {
+        "completion-menu.completion": "bg:#008888 #ffffff",
+        "completion-menu.completion.current": "bg:#00aaaa #000000",
+        "scrollbar.background": "bg:#88aaaa",
+        "scrollbar.button": "bg:#222222",
+    }
+)
+
+
 class CustomCompleter(Completer):
-    def __init__(self, search_func):
+    def __init__(self, search_func: any):
         self.search = search_func
 
     def get_completions(self, document, complete_event):
         for item in self.search(document.text):
-            yield Completion(item, start_position=-len(document.text_before_cursor), style="bg:ansicyan fg:ansiblack")
+            yield Completion(
+                item,
+                start_position=-len(document.text_before_cursor),
+            )
 
 
 @dataclass
 class T:
-    """
-    docstring
-    """
 
     nosay: bool = False
     lang: str = "english"
     sounds: str = "uk"
-    current: str = "hello"
-    worker: object = requests_html.HTMLSession()
-    session: object = PromptSession()
+    online: bool = False
     host = "dictionary.cambridge.org"
-
     platform = check_platform()
 
     def __post_init__(self):
+
+        self.session = PromptSession(history=FileHistory((Path.home() / ".t_history").absolute()))
         self.main_url = f"https://{self.host}"
+        self.worker = requests_html.HTMLSession()
         self.worker.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
             "AMP-Same-Origin": "true",
@@ -52,6 +70,18 @@ class T:
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
         }
+        self.words_completer = WordCompleter(self.load_data())
+
+    def load_data(self):
+
+        if self.online:
+            return []
+
+        filename = BASE_DIR / "data" / "words_alpha.txt"
+        if not filename.exists():
+            print("🤔  File not found")
+            sys.exit(0)
+        return filename.open("r").readlines()
 
     def play_voice(self, url: str) -> None:
         try:
@@ -69,10 +99,11 @@ class T:
         if not (
             word := self.session.prompt(
                 "> ",
-                completer=CustomCompleter(self.associate_dictionary),
+                completer=CustomCompleter(self.associate_dictionary) if self.online else self.words_completer,
                 auto_suggest=AutoSuggestFromHistory(),
-                # enable_history_search=True,
-                # complete_while_typing=True,
+                enable_history_search=True,
+                complete_while_typing=True,
+                style=style,
             ).strip()
         ):
             return
@@ -132,6 +163,7 @@ class T:
 
 @click.command()
 @click.option("-n", "--nosay", is_flag=True)
+@click.option("--online/--offline", default=True)
 @click.option(
     "-l",
     "--sounds",
